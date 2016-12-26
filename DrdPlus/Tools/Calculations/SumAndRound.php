bbe1ade0d925a192a96f605564d6bc6b7d6ead84
Jaroslav Týc <mail@jaroslavtyc.com>
2016-12-26 10:20:31 +0100

    Removed unused library dependencies

--- DrdPlus/Tools/Calculations/SumAndRound.php
+++ DrdPlus/Tools/Calculations/SumAndRound.php
@@ -8,102 +8,102 @@ class SumAndRound
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function round($number)
     {
-        return intval(round(ToFloat::toFloat($number)));
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
+        return (int)round(ToFloat::toFloat($number));
     }
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function floor($number)
     {
-        return intval(floor(ToFloat::toFloat($number)));
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
+        return (int)floor(ToFloat::toFloat($number));
     }
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function ceil($number)
     {
-        return intval(ceil(ToFloat::toFloat($number)));
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
+        return (int)ceil(ToFloat::toFloat($number));
     }
 
     /**
      * @param number $firstNumber
      * @param number $secondNumber
-     *
      * @return int
      */
     public static function average($firstNumber, $secondNumber)
     {
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
         return self::round((ToFloat::toFloat($firstNumber) + ToFloat::toFloat($secondNumber)) / 2);
     }
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function half($number)
     {
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
         return self::round(ToFloat::toFloat($number) / 2);
     }
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function flooredHalf($number)
     {
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
         return self::floor(ToFloat::toFloat($number) / 2);
     }
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function ceiledHalf($number)
     {
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
         return self::ceil(ToFloat::toFloat($number) / 2);
     }
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function third($number)
     {
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
         return self::round(ToFloat::toFloat($number) / 3);
     }
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function flooredThird($number)
     {
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
         return self::floor(ToFloat::toFloat($number) / 3);
     }
 
     /**
      * @param number $number
-     *
      * @return int
      */
     public static function ceiledThird($number)
     {
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */
         return self::ceil(ToFloat::toFloat($number) / 3);
     }
 }
